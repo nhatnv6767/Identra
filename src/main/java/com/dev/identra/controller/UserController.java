6ea7bf4efc79819a3581fb81ac92b182a7d5a2bb
bhnone <>
2024-10-20 23:28:46 +0900

    Service - > Controller

---
+++ src/main/java/com/dev/identra/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.dev.identra.controller;
+
+import com.dev.identra.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+
+}
--- src/main/java/com/dev/identra/service/UserService.java
+++ src/main/java/com/dev/identra/service/UserService.java
@@ -1,5 +1,6 @@
 package com.dev.identra.service;
 
+import com.dev.identra.dto.request.UserCreationRequest;
 import com.dev.identra.entity.User;
 import com.dev.identra.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User createRequest(Object request) {
-
+    public User createRequest(UserCreationRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+        
+        return userRepository.save(user);
     }
 }
