cff8d29a1747b16346ee7005c627efbebdd10bf3
bhnone <>
2024-10-22 21:09:17 +0900

    ErrorCode Exception #7

--- src/main/java/com/dev/identra/dto/request/UserCreationRequest.java
+++ src/main/java/com/dev/identra/dto/request/UserCreationRequest.java
@@ -5,7 +5,8 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 
 public class UserCreationRequest {
-    @Size(min = 3, message = "Username must be at least 3 characters")
+    // yêu cầu truyền vào 1 constant - chính là key của ErrorCode (enum)
+    @Size(min = 3, message = "USERNAME_INVALID")
     private String username;
     @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
