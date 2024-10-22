233e42368983f274bf42232aebf6389abbf4eded
bhnone <>
2024-10-22 21:13:01 +0900

    ErrorCode Exception #9

--- src/main/java/com/dev/identra/exception/ErrorCode.java
+++ src/main/java/com/dev/identra/exception/ErrorCode.java
@@ -3,7 +3,9 @@ package com.dev.identra.exception;
 public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Something went wrong - UNCATEGORIZED_EXCEPTION"),
     USER_EXISTED(1002, "Username already exists"),
-    USERNAME_INVALID(1003, "Username must be at least 3 characters");
+    USERNAME_INVALID(1003, "Username must be at least 3 characters"),
+    INVALID_PASSWORD(1004, "Password must be at least 8 characters"),
+    ;
 
 
     private int code;
