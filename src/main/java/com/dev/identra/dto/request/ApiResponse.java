271e8000cef617ceb9ee3da83a15983098215323
bhnone <>
2024-10-23 01:44:43 +0900

    bcrypt fix createUser why null

--- src/main/java/com/dev/identra/dto/request/ApiResponse.java
+++ src/main/java/com/dev/identra/dto/request/ApiResponse.java
@@ -4,14 +4,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-// Khai bao cho Json biet rang la khi serizlize object nay sang json thi nhung
-// cai nao null thi no ko fill vao json
-@JsonInclude(JsonInclude.Include.NON_NULL)
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+// Khai bao cho Json biet rang la khi serizlize object nay sang json thi nhung
+// cai nao null thi no ko fill vao json
 public class ApiResponse<T> {
     int code = 1000;
     String message;
