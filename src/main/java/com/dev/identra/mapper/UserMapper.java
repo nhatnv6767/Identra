44144ac0c903120f7bd81684ae3caac3130183c9
bhnone <>
2024-10-22 22:32:59 +0900

    Lombok and Mapstruct #16 when have no same property

--- src/main/java/com/dev/identra/mapper/UserMapper.java
+++ src/main/java/com/dev/identra/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import com.dev.identra.dto.request.UserUpdateRequest;
 import com.dev.identra.dto.response.UserResponse;
 import com.dev.identra.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 // Mapper để map dữ liệu từ UserCreationRequest về Object User
@@ -14,6 +15,8 @@ public interface UserMapper {
     // nhận về 1 cái parameter là request theo kiểu là UserCreationRequest và nó sẽ trả về 1 class là User
     User toUser(UserCreationRequest request);
 
+    // khi property khac nhau
+    @Mapping(source = "firstName", target = "lastName")
     UserResponse toUserResponse(User user);
 
     // map data từ UserUpdateRequest vào Object User
