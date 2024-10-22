b1ecfba54b2f792c7235c67414901f61e79fb36d
bhnone <>
2024-10-22 21:22:47 +0900

    ErrorCode Exception #11

--- src/main/java/com/dev/identra/exception/ErrorCode.java
+++ src/main/java/com/dev/identra/exception/ErrorCode.java
@@ -2,7 +2,7 @@ package com.dev.identra.exception;
 
 public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Something went wrong - UNCATEGORIZED_EXCEPTION"),
-    INVALID_KEY(1001, "Invalid message key"),
+    INVALID_KEY(1001, "Something went wrong - UNCATEGORIZED_EXCEPTION"),
     USER_EXISTED(1002, "Username already exists"),
     USERNAME_INVALID(1003, "Username must be at least 3 characters"),
     INVALID_PASSWORD(1004, "Password must be at least 8 characters"),
