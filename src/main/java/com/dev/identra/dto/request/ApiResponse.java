cab76ff895bfec6d949aaf970e9a85e33e0830af
bhnone <>
2024-10-22 21:54:54 +0900

    Lombok and Mapstruct #7 FieldDefaults Private

--- src/main/java/com/dev/identra/dto/request/ApiResponse.java
+++ src/main/java/com/dev/identra/dto/request/ApiResponse.java
@@ -1,36 +1,20 @@
 package com.dev.identra.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 // Khai bao cho Json biet rang la khi serizlize object nay sang json thi nhung
 // cai nao null thi no ko fill vao json
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ApiResponse<T> {
-    private int code = 1000;
-    private String message;
-    private T result;
+    int code = 1000;
+    String message;
+    T result;
 
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public T getResult() {
-        return result;
-    }
-
-    public void setResult(T result) {
-        this.result = result;
-    }
 }
