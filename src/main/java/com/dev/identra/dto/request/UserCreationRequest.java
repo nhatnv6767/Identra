160205c1d9834b44f7ca8cd8cd78c4f610d40e7d
bhnone <>
2024-10-22 21:48:02 +0900

    Lombok and Mapstruct #3

--- src/main/java/com/dev/identra/dto/request/UserCreationRequest.java
+++ src/main/java/com/dev/identra/dto/request/UserCreationRequest.java
@@ -1,13 +1,13 @@
 package com.dev.identra.dto.request;
 
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Getter
-@Setter
+@Data
 public class UserCreationRequest {
     // yêu cầu truyền vào 1 constant - chính là key của ErrorCode (enum)
     @Size(min = 3, message = "USERNAME_INVALID")
