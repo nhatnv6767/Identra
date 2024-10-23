305a3709df1e753429b91cd4e7f5c1bd688ddcc3
bhnone <>
2024-10-23 23:04:37 +0900

    Encrypt password #2

--- src/main/java/com/dev/identra/controller/AuthenticationController.java
+++ src/main/java/com/dev/identra/controller/AuthenticationController.java
@@ -1,8 +1,17 @@
 package com.dev.identra.controller;
 
+import com.dev.identra.entity.User;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
+// Autowired cac bean
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class AuthenticationController {
-
+    
 }
