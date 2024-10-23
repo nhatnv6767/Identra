c442eaba444ce121de37de3907ea5972966062df
bhnone <>
2024-10-23 23:14:39 +0900

    Encrypt password #7

--- src/main/java/com/dev/identra/controller/AuthenticationController.java
+++ src/main/java/com/dev/identra/controller/AuthenticationController.java
@@ -1,9 +1,15 @@
 package com.dev.identra.controller;
 
+import com.dev.identra.dto.request.ApiResponse;
+import com.dev.identra.dto.request.AuthenticationRequest;
+import com.dev.identra.dto.response.AuthenticationResponse;
 import com.dev.identra.entity.User;
+import com.dev.identra.service.AuthenticationService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class AuthenticationController {
-    
+    AuthenticationService authenticationService;
+    ;
+
+    @PostMapping("/log-in")
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        boolean result = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(AuthenticationResponse.builder().authenticated(result).build())
+
+                .build();
+    }
 }
