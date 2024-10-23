634f24d0922855765e441892dc059b6cde9b847b
bhnone <>
2024-10-23 23:10:52 +0900

    Encrypt password #6

--- src/main/java/com/dev/identra/service/AuthenticationService.java
+++ src/main/java/com/dev/identra/service/AuthenticationService.java
@@ -8,6 +8,8 @@ import com.dev.identra.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +20,8 @@ public class AuthenticationService {
 
     boolean authenticate(AuthenticationRequest request) {
         var user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        return passwordEncoder.matches(request.getPassword(), user.getPassword());
 
     }
 }
