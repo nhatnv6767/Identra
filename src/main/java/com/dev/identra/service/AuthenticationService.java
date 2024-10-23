9ff3d6cd6f24f815949b69d56fc0ab9d7663ee5b
bhnone <>
2024-10-23 23:08:55 +0900

    Encrypt password #4

---
+++ src/main/java/com/dev/identra/service/AuthenticationService.java
@@ -0,0 +1,23 @@
+package com.dev.identra.service;
+
+import com.dev.identra.dto.request.AuthenticationRequest;
+import com.dev.identra.dto.response.UserResponse;
+import com.dev.identra.exception.AppException;
+import com.dev.identra.exception.ErrorCode;
+import com.dev.identra.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationService {
+    UserRepository userRepository;
+
+    boolean authenticate(AuthenticationRequest request) {
+        var user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> AppException(ErrorCode.));
+
+    }
+}
