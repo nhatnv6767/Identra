ce4c1f0e91e16c813e42bc03d10b840808fae9ce
bhnone <>
2024-10-24 22:40:56 +0900

    #7. JWT 1

--- src/main/java/com/dev/identra/service/AuthenticationService.java
+++ src/main/java/com/dev/identra/service/AuthenticationService.java
@@ -1,6 +1,7 @@
 package com.dev.identra.service;
 
 import com.dev.identra.dto.request.AuthenticationRequest;
+import com.dev.identra.dto.response.AuthenticationResponse;
 import com.dev.identra.dto.response.UserResponse;
 import com.dev.identra.exception.AppException;
 import com.dev.identra.exception.ErrorCode;
@@ -8,20 +9,25 @@ import com.dev.identra.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
     UserRepository userRepository;
 
-    public boolean authenticate(AuthenticationRequest request) {
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
         var user = userRepository.findByUsername(request.getUsername()).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-        return passwordEncoder.matches(request.getPassword(), user.getPassword());
+        boolean authenticated =  passwordEncoder.matches(request.getPassword(), user.getPassword());
+        if(!authenticated){
+            throw new AppException()
+        }
 
     }
 }
