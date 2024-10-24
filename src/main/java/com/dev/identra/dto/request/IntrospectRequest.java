a21759d6bd8a2bde3902ef3647065ffd6e2b3371
bhnone <>
2024-10-24 23:12:33 +0900

    #7. JWT 8 verify token

---
+++ src/main/java/com/dev/identra/dto/request/IntrospectRequest.java
@@ -0,0 +1,15 @@
+package com.dev.identra.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class IntrospectRequest {
+    String token;
+}
+
+// dung de verify token
