4c36df8039e5206a92935225b4649ebccb76f5b9
bhnone <>
2024-10-24 22:50:57 +0900

    #7. JWT 4

--- src/main/java/com/dev/identra/service/AuthenticationService.java
+++ src/main/java/com/dev/identra/service/AuthenticationService.java
@@ -6,6 +6,11 @@ import com.dev.identra.dto.response.UserResponse;
 import com.dev.identra.exception.AppException;
 import com.dev.identra.exception.ErrorCode;
 import com.dev.identra.repository.UserRepository;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSObject;
+import com.nimbusds.jose.Payload;
+import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -13,6 +18,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +38,26 @@ public class AuthenticationService {
             throw new AppException(ErrorCode.UNAUTHENTICATED);
         }
 
+
+    }
+
+    private String generateToken(String username) {
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                // dai dien cho user dang nhap
+                .subject(username)
+                // chi dinh token issue tu ai?
+                .issuer("dev.identra")
+                .issueTime(new Date())
+                // het han sau 1 h
+                .expirationTime(new Date(
+                        Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
+                ))
+                .claim("customClaim", "Custom")
+                .build();
+        // payload nhan vao JSONObject
+        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
+        JWSObject jwsObject = new JWSObject(header, payload);
 
     }
 }
