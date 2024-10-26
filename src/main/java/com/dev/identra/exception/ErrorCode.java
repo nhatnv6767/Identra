23b92a1321f07b2d615aba13a3eb8deb9473b4b1
bhnone <>
2024-10-26 09:28:17 +0900

    #11. Exception management - return Response with HttpCode custom - 1

--- src/main/java/com/dev/identra/exception/ErrorCode.java
+++ src/main/java/com/dev/identra/exception/ErrorCode.java
@@ -1,29 +1,29 @@
 package com.dev.identra.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
 public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999, "Something went wrong - UNCATEGORIZED_EXCEPTION"),
-    INVALID_KEY(1001, "Something went wrong - UNCATEGORIZED_EXCEPTION"),
-    USER_EXISTED(1002, "Username already exists"),
-    USERNAME_INVALID(1003, "Username must be at least 3 characters"),
-    INVALID_PASSWORD(1004, "Password must be at least 8 characters"),
-    USER_NOT_EXISTED(1005, "User not existed"),
-    UNAUTHENTICATED(1006, "Unauthenticated"),
+    UNCATEGORIZED_EXCEPTION(9999, "Something went wrong - UNCATEGORIZED_EXCEPTION", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_KEY(1001, "Something went wrong - UNCATEGORIZED_EXCEPTION", HttpStatus.BAD_REQUEST),
+    USER_EXISTED(1002, "Username already exists", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1004, "Password must be at least 8 characters", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1005, "User not existed", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
     ;
 
 
     private int code;
     private String message;
+    private HttpStatusCode statusCode;
 
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
