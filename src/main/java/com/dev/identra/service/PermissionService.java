fca205ca37b1b2ffe1b58e70669a8569742de51c
bhnone <>
2024-10-26 11:44:22 +0900

    #12. Authorized with role, permission, Annotation ManyToMany in JPA 10

--- src/main/java/com/dev/identra/service/PermissionService.java
+++ src/main/java/com/dev/identra/service/PermissionService.java
@@ -3,6 +3,7 @@ package com.dev.identra.service;
 import com.dev.identra.dto.request.PermissionRequest;
 import com.dev.identra.dto.response.PermissionResponse;
 import com.dev.identra.entity.Permission;
+import com.dev.identra.mapper.PermissionMapper;
 import com.dev.identra.repository.PermissionRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -10,14 +11,28 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class PermissionService {
     PermissionRepository permissionRepository;
+    PermissionMapper permissionMapper;
 
-    PermissionResponse create(PermissionRequest request) {
-        Permission permission;
+    public PermissionResponse create(PermissionRequest request) {
+        Permission permission = permissionMapper.toPermission(request);
+        permission = permissionRepository.save(permission);
+        return permissionMapper.toPermissionResponse(permission);
+    }
+
+    public List<PermissionResponse> getAll() {
+        var permissions = permissionRepository.findAll();
+        return permissions.stream().map(permissionMapper::toPermissionResponse).toList();
+    }
+
+    public void delete(String permission) {
+        permissionRepository.deleteById(permission);
     }
 }
