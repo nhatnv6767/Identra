8d620d73ec7ae6f0f4927755fe853d04c535fbd3
bhnone <>
2024-10-26 11:37:19 +0900

    #12. Authorized with role, permission, Annotation ManyToMany in JPA 8

--- src/main/java/com/dev/identra/service/PermissionService.java
+++ src/main/java/com/dev/identra/service/PermissionService.java
@@ -1,5 +1,8 @@
 package com.dev.identra.service;
 
+import com.dev.identra.dto.request.PermissionRequest;
+import com.dev.identra.dto.response.PermissionResponse;
+import com.dev.identra.entity.Permission;
 import com.dev.identra.repository.PermissionRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +17,7 @@ import org.springframework.stereotype.Service;
 public class PermissionService {
     PermissionRepository permissionRepository;
 
-    Object create(Object request) {
-
+    PermissionResponse create(PermissionRequest request) {
+        Permission permission;
     }
 }
