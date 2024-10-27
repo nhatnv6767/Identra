b1100cfd749a5490fd4eb730df25bd70c12da012
bhnone <>
2024-10-27 22:00:42 +0900

    #12. Authorized with role, permission, Annotation ManyToMany in JPA 18

---
+++ src/main/java/com/dev/identra/controller/RoleController.java
@@ -0,0 +1,45 @@
+package com.dev.identra.controller;
+
+import com.dev.identra.dto.request.ApiResponse;
+import com.dev.identra.dto.request.PermissionRequest;
+import com.dev.identra.dto.request.RoleRequest;
+import com.dev.identra.dto.response.PermissionResponse;
+import com.dev.identra.dto.response.RoleResponse;
+import com.dev.identra.service.PermissionService;
+import com.dev.identra.service.RoleService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/roles")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class RoleController {
+    RoleService roleService;
+
+    @PostMapping
+    ApiResponse<RoleResponse> create(@RequestBody RoleRequest request) {
+        return ApiResponse.<RoleResponse>builder()
+                .result(roleService.create(request))
+                .build();
+    }
+
+    @GetMapping
+    ApiResponse<List<RoleResponse>> getAll() {
+        return ApiResponse.<List<RoleResponse>>builder()
+                .result(roleService.getAll())
+                .build();
+    }
+
+    @DeleteMapping("/{role}")
+    ApiResponse<Void> delete(@PathVariable String role) {
+        roleService.delete(role);
+        return ApiResponse.<Void>builder().build();
+    }
+}
