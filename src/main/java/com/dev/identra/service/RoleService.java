125d890d683bb6ddc9ea771358eefdb7f2497fed
bhnone <>
2024-10-27 21:57:39 +0900

    #12. Authorized with role, permission, Annotation ManyToMany in JPA 16

--- src/main/java/com/dev/identra/service/RoleService.java
+++ src/main/java/com/dev/identra/service/RoleService.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +30,10 @@ public class RoleService {
         role = roleRepository.save(role);
         return roleMapper.toRoleResponse(role);
 
+    }
+
+    public List<RoleResponse> getAll() {
+        var roles = roleRepository.findAll();
+        return roles.stream().map(roleMapper::toRoleResponse).toList();
     }
 }
